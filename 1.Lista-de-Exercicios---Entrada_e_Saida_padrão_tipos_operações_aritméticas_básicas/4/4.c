0dcaf330510db58c375f0510bc363604b2da1d1e
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2020-03-04 11:36:48 -0300

    Exercise 4

---
+++ 1.Lista-de-Exercicios---Entrada_e_Saida_padrão_tipos_operações_aritméticas_básicas/4/4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    /* Alterar a variável para float 
+        ou a saída dela para inteiro */
+
+    float aux;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%f", &aux);
+    printf("%f", aux);
+    return 0;
+}
