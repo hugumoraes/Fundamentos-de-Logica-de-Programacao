1c76a820e589cd18bde415318e99d819b80cd2bc
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2020-03-09 08:24:37 -0300

    Versão final

--- 1.Lista-de-Exercicios---Entrada_e_Saida_padrão_tipos_operações_aritméticas_básicas/1/1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main () {
-    printf("Oi! Esse %c um programa teste!\n*\n***\n*****\n\n" ,130);
-    printf("Oi! Esse %c um programa teste!\n  *  \n *** \n*****\n" ,130);
-    
-    return 0;
-}
--- 1.Lista-de-Exercicios---Entrada_e_Saida_padrão_tipos_operações_aritméticas_básicas/9/9.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main () {
-    int numero;
-
-    printf("Digite um n%cmero inteiro: " ,163);
-    scanf("%d", &numero);
-    printf("Seu n%cmero %c: %03d" ,163 ,130 ,numero);
-}
